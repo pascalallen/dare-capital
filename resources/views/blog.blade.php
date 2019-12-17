1abf5ff2134c833717884ab8aac212412edd6d59
Pascal Allen <pascal@onlinemeded.org>
2019-12-16 18:20:56 -0600

    update blog loop vars

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -13,15 +13,15 @@
         <div class="row justify-content-center m-lg-5 p-sm-3">
 
             <div class="col-lg-4">
-                @foreach($posts['case-studies'] as $post)
+                @foreach($posts['case-studies'] as $caseStudy)
                     <div class="row">
                         <div class="card bg-dark text-white">
-                            <img class="card-img" src="{{ asset('storage/images/'.$post->image) }}"
-                                 alt="{{ $post->image }}">
+                            <img class="card-img" src="{{ asset('storage/images/'.$caseStudy->image) }}"
+                                 alt="{{ $caseStudy->image }}">
                             <div class="card-img-overlay">
-                                <h5 class="card-title">{{ $post->title }}</h5>
-                                <p class="card-text">{{ $post->body }}</p>
-                                <p class="card-text">{{ $post->user->name }} on {{ $post->created_at }}</p>
+                                <h5 class="card-title">{{ $caseStudy->title }}</h5>
+                                <p class="card-text">{{ $caseStudy->body }}</p>
+                                <p class="card-text">{{ $caseStudy->user->name }} on {{ $caseStudy->created_at }}</p>
                             </div>
                         </div>
                     </div>
@@ -32,12 +32,12 @@
                 @foreach($posts['education'] as $education)
                     <div class="row">
                         <div class="card bg-dark text-white">
-                            <img class="card-img" src="{{ asset('storage/images/'.$post->image) }}"
-                                 alt="{{ $post->image }}">
+                            <img class="card-img" src="{{ asset('storage/images/'.$education->image) }}"
+                                 alt="{{ $education->image }}">
                             <div class="card-img-overlay">
-                                <h5 class="card-title">{{ $post->title }}</h5>
-                                <p class="card-text">{{ $post->body }}</p>
-                                <p class="card-text">{{ $post->user->name }} on {{ $post->created_at }}</p>
+                                <h5 class="card-title">{{ $education->title }}</h5>
+                                <p class="card-text">{{ $education->body }}</p>
+                                <p class="card-text">{{ $education->user->name }} on {{ $education->created_at }}</p>
                             </div>
                         </div>
                     </div>
