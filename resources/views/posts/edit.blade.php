3f2e81c8c4d06df9a9a333cac791a230d59f0eff
Pascal Allen <pascal@onlinemeded.org>
2019-12-16 19:31:54 -0600

    add https to summernote cdn links

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,8 +2,8 @@
 
 @push('scripts')
     <!-- include summernote css/js -->
-    <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.css" rel="stylesheet">
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.js"></script>
 @endpush
 
 @section('content')
