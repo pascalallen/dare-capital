f175786d7e2d271650d63334e89ea9fe77e50c2d
Pascal Allen <pascal@onlinemeded.org>
2019-12-17 19:55:09 -0600

    add styling to blog page

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,18 +3,18 @@
 @section('content')
     <div class="container mw-100 m-0 mt-5">
 
-        <div class="row flex-column justify-content-center" id="landing-page-banner">
+        <div class="row flex-column justify-content-center mb-5" id="landing-page-banner">
             <div class="text-center col-4 offset-4">
-                <h1>Dare Capital</h1>
+                <h1 class="display-3">Dare Capital</h1>
                 <h4>Dare to dream.<br>Dare to build.<br>Dare to grow.<br>Dare to breathe.</h4>
             </div>
         </div>
 
-        <div class="row justify-content-center m-lg-5 p-sm-3">
+        <div class="row justify-content-center">
 
-            <div class="col-lg-4">
+            <div class="col-4">
                 @foreach($posts['case-studies'] as $caseStudy)
-                    <div class="row">
+                    <div class="row m-5">
                         <div class="card bg-dark text-white">
                             <img class="card-img" src="{{ asset('storage/images/'.$caseStudy->image) }}"
                                  alt="{{ $caseStudy->image }}">
@@ -28,9 +28,9 @@
                 @endforeach
             </div>
 
-            <div class="col-lg-4">
+            <div class="col-4">
                 @foreach($posts['education'] as $education)
-                    <div class="row">
+                    <div class="row m-5">
                         <div class="card bg-dark text-white">
                             <img class="card-img" src="{{ asset('storage/images/'.$education->image) }}"
                                  alt="{{ $education->image }}">
