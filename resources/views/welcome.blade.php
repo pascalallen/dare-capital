b801aea79e9610c8b76cc4c9b93296375a58a3e7
Pascal Allen <pascal@onlinemeded.org>
2020-01-15 20:18:37 -0600

    update font on landing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
         <div class="row text-light">
             <h1 class="display-5 mx-auto d-block">How We Help</h1>
             <div class="row justify-content-center mt-lg-1 p-sm-3 welcome-panel w-100">
-                <div class="col-md-4 welcome-panel-text px-5 py-5" style="background-color: #242363">
+                <div class="col-md-4 welcome-panel-text px-5 py-5" style="background-color: #242363; color: #a7a9ac">
                     <h3>Understand Your Goals</h3>
                     <p>What do you want? What do you envision your company, your life, looking like in five years? We
                         want to help you build your future and grow toward your goals.</p>
@@ -30,7 +30,7 @@
                 <div class="col-md-4 desktop-panel-image welcome-panel-image">
                     <img src="//via.placeholder.com/350" class="img-fluid float-left shadow-lg">
                 </div>
-                <div class="col-md-4 welcome-panel-text px-5 py-5" style="background-color: #242363">
+                <div class="col-md-4 welcome-panel-text px-5 py-5" style="background-color: #242363; color: #a7a9ac">
                     <h3>Document Your Path</h3>
                     <p>We’ll work with you to understand your past, present, and future. Have a detour you’re not proud
                         of? We can overcome most challenges and plot an achievable path forward.</p>
@@ -40,7 +40,7 @@
                 </div>
             </div>
             <div class="row justify-content-center mb-lg-5 p-sm-3 welcome-panel w-100">
-                <div class="col-md-4 welcome-panel-text px-5 py-5" style="background-color: #242363">
+                <div class="col-md-4 welcome-panel-text px-5 py-5" style="background-color: #242363; color: #a7a9ac">
                     <h3>Plan Your Future</h3>
                     <p>We’re not just here to help with your cashflow. We’re a resource you can depend on - whether the
                         help you need is financial - or just an introduction to the right person.</p>
