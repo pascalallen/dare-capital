6cf8c1b2348af9357b67d9de481febab17bfb383
Pascal Allen <pascal@onlinemeded.org>
2020-01-15 20:15:07 -0600

    update landing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
         <div class="row text-light">
             <h1 class="display-5 mx-auto d-block">How We Help</h1>
             <div class="row justify-content-center mt-lg-1 p-sm-3 welcome-panel w-100">
-                <div class="col-md-4 welcome-panel-text px-5 py-5" style="background-color: #a7a9ac">
+                <div class="col-md-4 welcome-panel-text px-5 py-5" style="background-color: #242363">
                     <h3>Understand Your Goals</h3>
                     <p>What do you want? What do you envision your company, your life, looking like in five years? We
                         want to help you build your future and grow toward your goals.</p>
@@ -30,7 +30,7 @@
                 <div class="col-md-4 desktop-panel-image welcome-panel-image">
                     <img src="//via.placeholder.com/350" class="img-fluid float-left shadow-lg">
                 </div>
-                <div class="col-md-4 welcome-panel-text px-5 py-5" style="background-color: #6d6e71">
+                <div class="col-md-4 welcome-panel-text px-5 py-5" style="background-color: #242363">
                     <h3>Document Your Path</h3>
                     <p>We’ll work with you to understand your past, present, and future. Have a detour you’re not proud
                         of? We can overcome most challenges and plot an achievable path forward.</p>
@@ -40,7 +40,7 @@
                 </div>
             </div>
             <div class="row justify-content-center mb-lg-5 p-sm-3 welcome-panel w-100">
-                <div class="col-md-4 welcome-panel-text px-5 py-5" style="background-color: #a7a9ac">
+                <div class="col-md-4 welcome-panel-text px-5 py-5" style="background-color: #242363">
                     <h3>Plan Your Future</h3>
                     <p>We’re not just here to help with your cashflow. We’re a resource you can depend on - whether the
                         help you need is financial - or just an introduction to the right person.</p>
@@ -58,20 +58,15 @@
         {{--            </div>--}}
         {{--        </div>--}}
 
-        <div class="row mt-lg-5 mb-5">
-            <div class="lower-landing-banner"></div>
-        </div>
+{{--        <div class="row mt-lg-5 mb-5">--}}
+{{--            <div class="lower-landing-banner"></div>--}}
+{{--        </div>--}}
 
-        <div class="row text-light">
-            <div class="col-lg-4 offset-lg-4">
+        <div class="row mb-5 text-light" id="apply-now-form">
+            <div class="cognito col-lg-6 offset-lg-3">
                 <h5 class="lead">Talk to us. Let us understand your goals.</h5>
                 <p>We want to help you grow and build. Leave us your name and number below, and one of our executives
                     will give you a call as soon as possible.</p>
-            </div>
-        </div>
-
-        <div class="row mb-5 text-light" id="apply-now-form">
-            <div class="cognito col-lg-4 offset-lg-4">
                 <script src="https://services.cognitoforms.com/s/xVHxdnmaP0uDY40yI9wkMw"></script>
                 <script>Cognito.load("forms", {id: "2"});</script>
             </div>
