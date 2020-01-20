210a6b14da42ec24664c3f598ebf8897b37da700
Pascal Allen <pascal@onlinemeded.org>
2020-01-19 18:54:15 -0600

    fix blog banner image on mobile

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('title')
+    Login
+@endsection
+
 @push('styles')
     <style>
         .footer {
--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,6 +4,16 @@
     Blog
 @endsection
 
+@push('styles')
+    <style>
+        @media (max-width: 576px) {
+            .banner-image {
+                height: 163px;
+            }
+        }
+    </style>
+@endpush
+
 @section('content')
     <div class="container mw-100 m-0 pb-5">
 
