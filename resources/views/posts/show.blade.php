4e0ea1b61ee02a5e8b49b1285cfaca12cafc2730
Pascal Allen <pascal@onlinemeded.org>
2020-01-20 19:20:45 -0600

    update blog.show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,31 +5,30 @@
 @endsection
 
 @section('content')
-    <div class="container mw-100 m-0 mt-5 pb-5">
+    <div class="container mw-100 m-0 pb-5">
 
-        <div class="row flex-column mb-5 bg-white" id="landing-page-banner">
-            <div class="col-2">
-                <img src="{{ asset('images/Dare_Logo_AdmiralBlue_RGB.png') }}" class="banner-image img-fluid"
-                     alt="Dare_Logo_AdmiralBlue_RGB.png"/>
-            </div>
-            <div class="text-center text-dark mb-4">
-                <h4>Dare to build.<br>Dare to grow.<br>Dare to give.</h4>
-            </div>
+        <div class="row mb-5">
+            <img src="{{ asset('/images/people-sunrise.jpg') }}" alt="Stories"
+                 class="banner-image"/>
         </div>
 
-        <h1 class="display-5 text-center text-light">{{ $post->title }}</h1>
-        <div class="row p-5">
-            <div class="col-4 offset-4">
-                <img class="img-fluid shadow-lg" src="{{ asset('storage/images/'.$post->image) }}"
-                     alt="{{ $post->image }}">
+        <div class="row mb-5 justify-content-center text-light">
+            <div class="col-md-4 my-auto">
+                <div class="card bg-light shadow-lg mb-2 border-0">
+                    <img class="card-img blog-index-image"
+                         src="{{ asset('storage/images/'.$post->image) }}"
+                         alt="{{ $post->image }}">
+                    <div class="card-img-overlay text-dark">
+                        <h1 class="display-5 card-title">{{ $post->title }}</h1>
+                    </div>
+                </div>
+                <blockquote class="blockquote card-text">
+                    {!! str_limit(strip_tags($post->body), $limit = 500, $end = '...') !!}
+                    <footer
+                        class="blockquote-footer text-right card-text mt-3">{{ $post->user->name }}
+                        on {{ date_format($post->created_at, 'M d, Y') }}</footer>
+                </blockquote>
             </div>
-        </div>
-        <div class="row text-light">
-            <blockquote class="blockquote col-4 offset-4">
-                {!! $post->body !!}
-                <footer class="blockquote-footer text-right">{{ $post->user->name }}
-                    on {{ date_format($post->created_at, 'M d, Y') }}</footer>
-            </blockquote>
         </div>
 
     </div>
