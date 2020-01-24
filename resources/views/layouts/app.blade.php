7bd1314fc19dfe433810fed25895560a79ec35e3
Pascal Allen <pascal@onlinemeded.org>
2020-01-24 15:51:39 -0600

    add mid section on landing page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -95,6 +95,124 @@ background-color: #3a888b;margin-left: 105px; margin-top: 68px;font-family: 'Nun
                     line-height: 30px;">Apply for Funding</button>
                 </div>
             </div>
+            <div class="row text-center" style="margin-left: 90px; margin-right: 90px">
+                <div class="col-md-4">
+                    <img src="{{ asset('images/shutterstock_1085127647.png') }}">
+                    <div style="
+                    margin-top: 25px;
+                    margin-bottom: 25px;
+                    color: #3a888b;
+                    font-family: 'Nunito', sans-serif;
+                    font-size: 29px;">DARE TO BUILD</div>
+                    <div style="height: 400px; padding: 30px; text-align: left; margin:auto;width: 85%; box-shadow: 0 10px 25px 1px rgba(20, 20, 80, 0.14);border-radius: 4px;">
+                        <div style="
+                            color: #011f4b;
+                            font-family: 'Nunito', sans-serif;
+                            font-size: 28px;
+                            font-weight: 300;
+                            ">A financing
+                            relationship you can
+                            <strong style="font-weight: 900">build the future</strong> on.</div>
+                        <div style="
+                        margin-top: 10px;
+                        color: #6e748e;
+                        font-family: 'Nunito', sans-serif;
+                        font-size: 21px;
+                        font-weight: 400;
+                        line-height: 27px;
+                        letter-spacing: 1.05px;">Have outstanding AR? Want a steadier cashflow? Need a line of credit, but you don’t fit in a bank’s box?
+                            We’re here to help, and we love a challenge.</div>
+                        <div style="
+                        width: 113px;
+                        height: 13px;
+                        color: #3c8685;
+                        font-family: 'Nunito', sans-serif;
+                        font-size: 16px;
+                        font-weight: 400;
+                        position: absolute;
+                        bottom: 50px;
+"><a>Read More >></a></div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <img src="{{ asset('images/shutterstock_1564227556.png') }}">
+                    <div style="
+                    margin-top: 25px;
+                    margin-bottom: 25px;
+                    color: #3a888b;
+                    font-family: 'Nunito', sans-serif;
+                    font-size: 29px;">DARE TO GROW</div>
+                    <div style="height: 400px; padding: 30px; text-align:left; margin:auto;width: 85%;box-shadow: 0 10px 25px 1px rgba(20, 20, 80, 0.14);border-radius: 4px;">
+                        <div style="
+                            color: #011f4b;
+                            font-family: 'Nunito', sans-serif;
+                            font-size: 28px;
+                            font-weight: 300;
+                            ">
+                            <strong style="font-weight: 900">A partnership</strong> you
+                            won’t have to sign
+                            away equity for.*</div>
+                        <div style="
+                        margin-top: 10px;
+                        color: #6e748e;
+                        font-family: 'Nunito', sans-serif;
+                        font-size: 21px;
+                        font-weight: 400;
+                        line-height: 27px;
+                        letter-spacing: 1.05px;">We can help you grow, the right way, at the right pace. With working capital financing, you don’t have to give up equity or mortgage your future−but you do get a financing partner invested in your success.
+                        </div>
+                        <div style="
+                        width: 113px;
+                        height: 13px;
+                        color: #3c8685;
+                        font-family: 'Nunito', sans-serif;
+                        font-size: 16px;
+                        font-weight: 400;
+                        position: absolute;
+                        bottom: 50px;"><a>Read More >></a></div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <img src="{{ asset('images/Vector_Smart_Object.png') }}">
+                    <div style="
+                    margin-top: 25px;
+                    margin-bottom: 25px;
+                    color: #3a888b;
+                    font-family: 'Nunito', sans-serif;
+                    font-size: 29px;">DARE TO GIVE</div>
+                    <div style="min-height: 400px; padding: 30px; text-align:left; margin:auto;width: 85%;box-shadow: 0 10px 25px 1px rgba(20, 20, 80, 0.14);border-radius: 4px;">
+                        <div style="
+                            color: #011f4b;
+                            font-family: 'Nunito', sans-serif;
+                            font-size: 28px;
+                            font-weight: 300;
+                            ">A resource
+                            you can rely on
+                            for
+                            <strong style="font-weight: 900">more than financing</strong>.</div>
+                        <div style="
+                        margin-top: 10px;
+                        color: #6e748e;
+                        font-family: 'Nunito', sans-serif;
+                        font-size: 21px;
+                        font-weight: 400;
+                        line-height: 27px;
+                        letter-spacing: 1.05px;">We’ve worked with 1,000+ companies in every stage of growth. We’ve seen it all;
+                            we know all the red flags, and we can usually help with obstacles large and small. </div>
+                        <div style="
+                        width: 113px;
+                        height: 13px;
+                        color: #3c8685;
+                        font-family: 'Nunito', sans-serif;
+                        font-size: 16px;
+                        font-weight: 400;
+                        position: absolute;
+                        bottom: 50px;
+                        "><a>Read More >></a></div>
+                    </div>
+                </div>
+
+            </div>
 {{--            <img src="{{ asset('images/austin-skyline.png') }}" alt="Austin Skyline" class="img-fluid w-100">--}}
 {{--            <img src="{{ asset('/images/shutterstock_451729549.png') }}" alt="Austin skyline" class="Austin skyline" />--}}
 {{--            @include('includes.navbar')--}}
