035a306f778a1b946565c8b43348677a147bb879
Pascal Allen <pascal@onlinemeded.org>
2020-01-25 01:01:06 -0600

    add lower section to welcome page

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,81 +1,30 @@
-<nav class="navbar navbar-expand-md nav-top fixed-top">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-        <span class="navbar-toggler-icon mt-2"><i class="fas fa-bars"></i></span>
-    </button>
-
-    <!-- Left Side Of Navbar -->
-    <ul class="navbar-nav nav-left">
-        <li class="nav-item nav-logo" style="width: 100px">
-            <a class="navbar-brand ml-1 mr-1" href="{{ url('/') }}">
-                <img
-                    src="{{ asset('images/Dare_Logo_AdmiralBlue_RGB.png') }}"
-                    class="img-fluid"
-                    alt="Dare_Logo_AdmiralBlue_RGB.png"
-                />
-            </a>
-        </li>
-    </ul>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item nav-tagline">
-                <a class="nav-link" href="{{ url('/') }}">{{ __('Dare to build. Dare to grow. Dare to give.') }}</a>
-            </li>
-        </ul>
-
-        <!-- Right Side Of Navbar -->
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="/#apply-now-form">{{ __('Apply Now') }}</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('about') }}">{{ __('About Us') }}</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('blog') }}">{{ __('Stories') }}</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://www.twitter.com/coledoescapital" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter"></i></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://www.facebook.com/DareBizCapital" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook"></i></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://www.linkedin.com/company/darecapital" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a>
-            </li>
-            @auth
-                <li class="nav-item dropdown">
-                    <a
-                        id="navbarDropdown"
-                        class="nav-link dropdown-toggle"
-                        href="#"
-                        role="button"
-                        data-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                    >
-                        {{ Auth::user()->name }} <span class="caret"></span>
-                    </a>
-
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('posts.index') }}">
-                            {{ __('Posts') }}
-                        </a>
-                        <a class="dropdown-item" href="{{ route('profile.index') }}">
-                            {{ __('Profile') }}
-                        </a>
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </div>
-                </li>
-            @endauth
-        </ul>
-    </div>
-</nav>
+<div style="float: right; margin-top: 62px;">
+    <a style="width: 75px;
+                    height: 15px;
+                    color: #ffffff;
+                    font-family: 'Nunito', sans-serif;
+                    font-size: 18px;
+                    font-weight: 400;
+                    line-height: 30px;
+                    margin-right: 50px;">About Us</a>
+    <a style="width: 75px;
+                    height: 15px;
+                    color: #ffffff;
+                    font-family: 'Nunito', sans-serif;
+                    font-size: 18px;
+                    font-weight: 400;
+                    line-height: 30px;margin-right: 50px;">Resources</a>
+    <a style="width: 75px;
+                    height: 15px;
+                    color: #ffffff;
+                    font-family: 'Nunito', sans-serif;
+                    font-size: 18px;
+                    font-weight: 400;
+                    line-height: 30px;margin-right: 50px;">Stories</a>
+    <a style="margin-right: 35px;font-size: x-large;
+color: #ffffff;" href="https://www.twitter.com/coledoescapital" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter"></i></a>
+    <a style="margin-right: 35px;font-size: x-large;
+color: #ffffff;" href="https://www.facebook.com/DareBizCapital" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
+    <a style="margin-right: 100px;font-size: x-large;
+color: #ffffff;" href="https://www.linkedin.com/company/darecapital" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,64 +1,297 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container mw-100 welcome-container">
-
-        @if (session('status'))
-            <div class="alert alert-success position-fixed mt-5 col-4 offset-4" style="z-index: 1">
-                {{ session('status') }}
+    <img style="margin-top: 62px; margin-left: 90px;" src="{{ asset('images/LOGO_Vector_Smart_Object.png') }}" alt="Dare Logo">
+    <div style="margin-top: 80px; margin-left: 102px;
+                width: 427px;
+                height: 102px;
+                color: #ffffff;
+                    font-family: 'Nunito', sans-serif;
+                font-size: 42px;
+                font-weight: 300;
+                line-height: 59px;">
+        Working capital
+        for growing companies.
+    </div>
+    <div>
+        <button style="width: 185px;
+height: 50px;
+box-shadow: 0 10px 25px 1px rgba(255, 159, 0, 0.14);
+background-color: #3a888b;margin-left: 105px; margin-top: 68px;font-family: 'Nunito', sans-serif;color: #ffffff;border: #3a888b;
+                    font-size: 18px;
+                    font-weight: 400;
+                    line-height: 30px;">Apply for Funding</button>
+    </div>
+    </div>
+    <div class="row text-center" style="margin-left: 90px; margin-right: 90px">
+        <div class="col-md-4">
+            <img src="{{ asset('images/shutterstock_1085127647.png') }}">
+            <div style="
+                    margin-top: 25px;
+                    margin-bottom: 25px;
+                    color: #3a888b;
+                    font-family: 'Nunito', sans-serif;
+                    font-size: 29px;">DARE TO BUILD</div>
+            <div style="height: 400px; padding: 30px; text-align: left; margin:auto;width: 85%; box-shadow: 0 10px 25px 1px rgba(20, 20, 80, 0.14);border-radius: 4px;">
+                <div style="
+                            color: #011f4b;
+                            font-family: 'Nunito', sans-serif;
+                            font-size: 28px;
+                            font-weight: 300;
+                            ">A financing
+                    relationship you can
+                    <strong style="font-weight: 900">build the future</strong> on.</div>
+                <div style="
+                        margin-top: 10px;
+                        color: #6e748e;
+                        font-family: 'Nunito', sans-serif;
+                        font-size: 21px;
+                        font-weight: 400;
+                        line-height: 27px;
+                        letter-spacing: 1.05px;">Have outstanding AR? Want a steadier cashflow? Need a line of credit, but you don’t fit in a bank’s box?
+                    We’re here to help, and we love a challenge.</div>
+                <div style="
+                        width: 113px;
+                        height: 13px;
+                        color: #3c8685;
+                        font-family: 'Nunito', sans-serif;
+                        font-size: 16px;
+                        font-weight: 400;
+                        position: absolute;
+                        bottom: 50px;
+"><a>Read More >></a></div>
             </div>
-        @endif
-
-        <div class="row mb-4">
-            <img src="{{ asset('/images/shutterstock_451729549.png') }}" alt="Austin skyline"
-                 class="banner-image"/>
+        </div>
+        <div class="col-md-4" style="bottom: 100px;">
+            <img src="{{ asset('images/shutterstock_1564227556.png') }}">
+            <div style="
+                    margin-top: 25px;
+                    margin-bottom: 25px;
+                    color: #3a888b;
+                    font-family: 'Nunito', sans-serif;
+                    font-size: 29px;">DARE TO GROW</div>
+            <div style="height: 400px; padding: 30px; text-align:left; margin:auto;width: 85%;box-shadow: 0 10px 25px 1px rgba(20, 20, 80, 0.14);border-radius: 4px;">
+                <div style="
+                            color: #011f4b;
+                            font-family: 'Nunito', sans-serif;
+                            font-size: 28px;
+                            font-weight: 300;
+                            ">
+                    <strong style="font-weight: 900">A partnership</strong> you
+                    won’t have to sign
+                    away equity for.*</div>
+                <div style="
+                        margin-top: 10px;
+                        color: #6e748e;
+                        font-family: 'Nunito', sans-serif;
+                        font-size: 21px;
+                        font-weight: 400;
+                        line-height: 27px;
+                        letter-spacing: 1.05px;">We can help you grow, the right way, at the right pace. With working capital financing, you don’t have to give up equity or mortgage your future−but you do get a financing partner invested in your success.
+                </div>
+                <div style="
+                        width: 113px;
+                        height: 13px;
+                        color: #3c8685;
+                        font-family: 'Nunito', sans-serif;
+                        font-size: 16px;
+                        font-weight: 400;
+                        position: absolute;
+                        bottom: 50px;"><a>Read More >></a></div>
+            </div>
+        </div>
+        <div class="col-md-4" style="bottom: 200px;">
+            <img src="{{ asset('images/Vector_Smart_Object.png') }}">
+            <div style="
+                    margin-top: 25px;
+                    margin-bottom: 25px;
+                    color: #3a888b;
+                    font-family: 'Nunito', sans-serif;
+                    font-size: 29px;">DARE TO GIVE</div>
+            <div style="min-height: 400px; padding: 30px; text-align:left; margin:auto;width: 85%;box-shadow: 0 10px 25px 1px rgba(20, 20, 80, 0.14);border-radius: 4px;">
+                <div style="
+                            color: #011f4b;
+                            font-family: 'Nunito', sans-serif;
+                            font-size: 28px;
+                            font-weight: 300;
+                            ">A resource
+                    you can rely on
+                    for
+                    <strong style="font-weight: 900">more than financing</strong>.</div>
+                <div style="
+                        margin-top: 10px;
+                        color: #6e748e;
+                        font-family: 'Nunito', sans-serif;
+                        font-size: 21px;
+                        font-weight: 400;
+                        line-height: 27px;
+                        letter-spacing: 1.05px;">We’ve worked with 1,000+ companies in every stage of growth. We’ve seen it all;
+                    we know all the red flags, and we can usually help with obstacles large and small. </div>
+                <div style="
+                        width: 113px;
+                        height: 13px;
+                        color: #3c8685;
+                        font-family: 'Nunito', sans-serif;
+                        font-size: 16px;
+                        font-weight: 400;
+                        position: absolute;
+                        bottom: 50px;
+                        "><a>Read More >></a></div>
+            </div>
         </div>
 
-        <div class="row text-light">
-            <h1 class="display-5 mx-auto">How We Help</h1>
-            <div class="row justify-content-center mt-lg-1 p-sm-3 welcome-panel w-100">
-                <div class="col-md-4 welcome-panel-text px-5 py-5" style="background-color: #242363; color: #a7a9ac">
-                    <h3>Understand Your Goals</h3>
-                    <p>What do you want? What do you envision your company, your life, looking like in five years? We
-                        want to help you build your future and grow toward your goals.</p>
-                </div>
-                <div class="col-md-4 welcome-panel-image">
-                    <img src="//via.placeholder.com/350" class="img-fluid float-right shadow-lg">
-                </div>
+    </div>
+    <img src="{{ asset('images/Vector_Smart_Object_Diagonal_Line_1.png') }}" class="w-100">
+    <div style="margin: auto; text-align: center; max-width: 50%">
+        <h1 style="
+                color: #6e748e;
+                font-family: 'Nunito', sans-serif;
+                font-size: 46px;
+                font-weight: 400;
+                margin: auto;
+                text-transform: uppercase;
+                letter-spacing: 3.45px;">INDUSTRIES WE SERVE</h1>
+        <div class="row" style="margin-top: 85px">
+            <div class="col-md-3">
+                <img src="{{ asset('images/Vector_Smart_Object (1).png') }}">
+                <div style="
+color: #31364f;
+font-family: 'Nunito', sans-serif;
+font-size: 28px;
+margin-top: 23px;
+font-weight: 300;">Manufacturing</div>
             </div>
-            <div class="row justify-content-center p-sm-3 welcome-panel w-100">
-                <div class="col-md-4 desktop-panel-image welcome-panel-image">
-                    <img src="//via.placeholder.com/350" class="img-fluid float-left shadow-lg">
-                </div>
-                <div class="col-md-4 welcome-panel-text px-5 py-5" style="background-color: #242363; color: #a7a9ac">
-                    <h3>Document Your Path</h3>
-                    <p>We’ll work with you to understand your past, present, and future. Have a detour you’re not proud
-                        of? We can overcome most challenges and plot an achievable path forward.</p>
-                </div>
-                <div class="col-md-4 mobile-panel-image welcome-panel-image">
-                    <img src="//via.placeholder.com/350" class="img-fluid float-left shadow-lg">
-                </div>
+            <div class="col-md-3">
+                <img src="{{ asset('images/Vector_Smart_Object (2).png') }}">
+                <div style="
+color: #31364f;
+font-family: 'Nunito', sans-serif;
+font-size: 28px;
+margin-top: 23px;
+font-weight: 300;">Wholesale
+                    Distributors</div>
             </div>
-            <div class="row justify-content-center mb-lg-5 p-sm-3 welcome-panel w-100">
-                <div class="col-md-4 welcome-panel-text px-5 py-5" style="background-color: #242363; color: #a7a9ac">
-                    <h3>Plan Your Future</h3>
-                    <p>We’re not just here to help with your cashflow. We’re a resource you can depend on - whether the
-                        help you need is financial - or just an introduction to the right person.</p>
-                </div>
-                <div class="col-md-4 welcome-panel-image">
-                    <img src="//via.placeholder.com/350" class="img-fluid float-right shadow-lg">
-                </div>
+            <div class="col-md-3">
+                <img src="{{ asset('images/Vector_Smart_Object (3).png') }}">
+                <div style="
+color: #31364f;
+font-family: 'Nunito', sans-serif;
+font-size: 28px;
+margin-top: 23px;
+font-weight: 300;">Oil & Gas
+                    Services</div>
+            </div>
+            <div class="col-md-3">
+                <img src="{{ asset('images/Vector_Smart_Object (4).png') }}">
+                <div style="
+color: #31364f;
+font-family: 'Nunito', sans-serif;
+font-size: 28px;
+margin-top: 23px;
+font-weight: 300;">B2B Services</div>
             </div>
         </div>
-        <div class="row mb-5 text-light" id="apply-now-form">
-            <div class="cognito col-lg-6 offset-lg-3">
-                <h5 class="lead">Talk to us. Let us understand your goals.</h5>
-                <p>We want to help you grow and build. Leave us your name and number below, and one of our executives
-                    will give you a call as soon as possible.</p>
-                <script src="https://services.cognitoforms.com/s/xVHxdnmaP0uDY40yI9wkMw"></script>
-                <script>Cognito.load("forms", {id: "2"});</script>
+        <div class="row" style="margin-top: 85px">
+            <div class="col-md-4">
+                <img src="{{ asset('images/Vector_Smart_Object (5).png') }}">
+                <div style="
+color: #31364f;
+font-family: 'Nunito', sans-serif;
+font-size: 28px;
+margin-top: 23px;
+font-weight: 300;">Construction</div>
+            </div>
+            <div class="col-md-4">
+                <img src="{{ asset('images/Vector_Smart_Object (6).png') }}">
+                <div style="
+color: #31364f;
+font-family: 'Nunito', sans-serif;
+font-size: 28px;
+margin-top: 23px;
+font-weight: 300;">Staffing</div>
+            </div>
+            <div class="col-md-4">
+                <img src="{{ asset('images/Vector_Smart_Object (7).png') }}">
+                <div style="
+color: #31364f;
+font-family: 'Nunito', sans-serif;
+font-size: 28px;
+margin-top: 23px;
+font-weight: 300;">IT Services</div>
             </div>
         </div>
     </div>
+    <img src="{{ asset('images/Vector_Smart_Object_Diagonal_Line_1.png') }}" class="w-100">
+    <h1 style="
+                color: #6e748e;
+                font-family: 'Nunito', sans-serif;
+                font-size: 46px;
+                font-weight: 400;
+                margin: auto;
+                text-align: center;
+                text-transform: uppercase;
+                letter-spacing: 3.45px;">WHAT PEOPLE<br>
+        ARE SAYING</h1>
+    <div class="row text-center" style="margin-left: 90px; margin-right: 90px; margin-top: 68px;">
 
+        <div class="col-md-4">
+            <div style="height: 315px; padding: 30px; text-align: left; margin:auto;width: 85%; box-shadow: 0 10px 25px 1px rgba(20, 20, 80, 0.14);border-radius: 4px;">
+                <div style="
+                color: #515b69;
+                font-family: 'Nunito', sans-serif;
+                font-size: 20px;
+                font-weight: 400;
+                line-height: 34px;
+                            ">Steven, CEO of CompanyXYZ</div>
+                <div style="
+                        margin-top: 10px;
+                        color: #6e748e;
+                        font-family: 'Nunito', sans-serif;
+                        font-size: 21px;
+                        font-weight: 400;
+                        line-height: 27px;
+                        letter-spacing: 1.05px;">“We love that Dare works with you to understand your past, present, and future. Have a detour you’re not proud of? We can overcome most challenges and plot an achievable path forward.”</div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div style="height: 315px; padding: 30px; text-align: left; margin:auto;width: 85%; box-shadow: 0 10px 25px 1px rgba(20, 20, 80, 0.14);border-radius: 4px;">
+                <div style="
+                color: #515b69;
+                font-family: 'Nunito', sans-serif;
+                font-size: 20px;
+                font-weight: 400;
+                line-height: 34px;
+                            ">Steven, CEO of CompanyXYZ</div>
+                <div style="
+                        margin-top: 10px;
+                        color: #6e748e;
+                        font-family: 'Nunito', sans-serif;
+                        font-size: 21px;
+                        font-weight: 400;
+                        line-height: 27px;
+                        letter-spacing: 1.05px;">“We love that Dare works with you to understand your past, present, and future. Have a detour you’re not proud of? We can overcome most challenges and plot an achievable path forward.”</div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div style="height: 315px; padding: 30px; text-align: left; margin:auto;width: 85%; box-shadow: 0 10px 25px 1px rgba(20, 20, 80, 0.14);border-radius: 4px;">
+                <div style="
+                color: #515b69;
+                font-family: 'Nunito', sans-serif;
+                font-size: 20px;
+                font-weight: 400;
+                line-height: 34px;
+                            ">Steven, CEO of CompanyXYZ</div>
+                <div style="
+                        margin-top: 10px;
+                        color: #6e748e;
+                        font-family: 'Nunito', sans-serif;
+                        font-size: 21px;
+                        font-weight: 400;
+                        line-height: 27px;
+                        letter-spacing: 1.05px;
+              ">“We love that Dare works with you to understand your past, present, and future. Have a detour you’re not proud of? We can overcome most challenges and plot an achievable path forward.”</div>
+            </div>
+        </div>
+
+    </div>
 @endsection
