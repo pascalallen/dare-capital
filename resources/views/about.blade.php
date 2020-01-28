8fd99cd6b5eb76f9c756ab85908c14e57c28e5e0
Pascal Allen <pascal@onlinemeded.org>
2020-01-27 20:17:04 -0600

    mobile text on about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -24,18 +24,19 @@ background-color: #ffffff;">
                 font-weight: 900;
                 line-height: 30px;
                 text-transform: uppercase;
-                letter-spacing: 0.87px;">WHO WE ARE</div>
+                letter-spacing: 0.87px;">WHO WE ARE
+                </div>
                 <div style="
                 margin-top: 25px;
                 color: #011f4b;
                 font-family: 'Nunito', sans-serif;
-                font-size: 28px;
-                font-weight: 300;
-                line-height: 33px;
-                letter-spacing: 0.28px;">
+                font-size: 2.5vh;
+                font-weight: 300;">
                     <span style="
                 font-weight: 900;">We’re the financial partner you’ve always wanted on your team. </span>
-                    Dare Capital is a commercial lending company founded on the principle of giving−giving trust with transparency, giving empathy with solutions, and giving support with the promise of a long-term commitment to your success.
+                    Dare Capital is a commercial lending company founded on the principle of giving−giving trust with
+                    transparency, giving empathy with solutions, and giving support with the promise of a long-term
+                    commitment to your success.
                 </div>
             </div>
         </div>
@@ -51,16 +52,19 @@ background-color: #ffffff;">
                 font-weight: 900;
                 line-height: 30px;
                 text-transform: uppercase;
-                letter-spacing: 0.87px;">WHAT WE DO</div>
+                letter-spacing: 0.87px;">WHAT WE DO
+                </div>
                 <div style="
                 margin-top: 25px;
                 color: #011f4b;
                 font-family: 'Nunito', sans-serif;
-                font-size: 28px;
-                font-weight: 300;
-                line-height: 33px;
-                letter-spacing: 0.28px;">
-                    Does your company have outstanding invoices? What about equipment, inventory, or other assets needed to grow your business? Need to cover payroll? That’s where we come in−but that’s just the beginning. <span style="font-weight: 900;">We’ll be there to help you grow and scale; we’ll be there when you need advice or an introduction; and when there are problems, we strive to be your first call.</span> We’ll always tell you the truth−good news and bad news−and share everything that informs our decision making.
+                font-size: 2.5vh;
+                font-weight: 300;">
+                    Does your company have outstanding invoices? What about equipment, inventory, or other assets needed
+                    to grow your business? Need to cover payroll? That’s where we come in−but that’s just the beginning.
+                    <span style="font-weight: 900;">We’ll be there to help you grow and scale; we’ll be there when you need advice or an introduction; and when there are problems, we strive to be your first call.</span>
+                    We’ll always tell you the truth−good news and bad news−and share everything that informs our
+                    decision making.
                 </div>
             </div>
         </div>
@@ -76,16 +80,21 @@ background-color: #ffffff;">
                 font-weight: 900;
                 line-height: 30px;
                 text-transform: uppercase;
-                letter-spacing: 0.87px;">WHY WE DO IT</div>
+                letter-spacing: 0.87px;">WHY WE DO IT
+                </div>
                 <div style="
                 margin-top: 25px;
                 color: #011f4b;
                 font-family: 'Nunito', sans-serif;
-                font-size: 28px;
-                font-weight: 300;
-                line-height: 33px;
-                letter-spacing: 0.28px;">
-                    <span style="font-weight: 900;">We’re really good at it! We’ve grown thousands of companies, most of whom have worked with us for years. And we know what it takes.</span> We’ve seen that successful businesses are built from a giving mindset, by courageous entrepreneurs who know their vulnerabilities, own their failures, and turn both into their biggest strengths. We've seen that the best measure of long-term success is in how a business treats its smallest partner. In giving, entrepreneurs foster trust and build relationships. When they dare to grow, we can provide the financial platform and trusted partnership they need to build the future that achieves their goals.
+                font-size: 2.5vh;
+                font-weight: 300;">
+                    <span style="font-weight: 900;">We’re really good at it! We’ve grown thousands of companies, most of whom have worked with us for years. And we know what it takes.</span>
+                    We’ve seen that successful businesses are built from a giving mindset, by courageous entrepreneurs
+                    who know their vulnerabilities, own their failures, and turn both into their biggest strengths.
+                    We've seen that the best measure of long-term success is in how a business treats its smallest
+                    partner. In giving, entrepreneurs foster trust and build relationships. When they dare to grow, we
+                    can provide the financial platform and trusted partnership they need to build the future that
+                    achieves their goals.
                 </div>
             </div>
         </div>
@@ -115,17 +124,20 @@ background-color: #ffffff;">
                 line-height: 35px;
                 font-weight: 900;
                 letter-spacing: 0.84px;">COLE HARMONSON<br>
-                <span style="font-weight: 400;letter-spacing: 0.84px;">co-founder and CEO</span>
-            </div>
+                    <span style="font-weight: 400;letter-spacing: 0.84px;">co-founder and CEO</span>
+                </div>
                     </div>
                 </div>
                 <div style="margin-top: 35px;
                 color: #011f4b;
                 font-family: 'Nunito', sans-serif;
-                font-size: 25px;
-                font-weight: 400;
-                line-height: 37px;
-                letter-spacing: -0.25px;">From the very beginning of his career, Cole has recognized the transformative value of factoring and asset-based lending to manage cashflow for growing companies without impacting equity. In 1996, he founded State Bank’s factoring and asset based lending business; when they sold their business in 2007, Cole took the leap into entrepreneurship and founded Far West Capital, which had a successful bank exit in 2018 after supporting hundreds of companies’ growth. </div>
+                font-size: 2.5vh;
+                font-weight: 400;">From the very beginning of his career, Cole has recognized the transformative value
+                    of factoring and asset-based lending to manage cashflow for growing companies without impacting
+                    equity. In 1996, he founded State Bank’s factoring and asset based lending business; when they sold
+                    their business in 2007, Cole took the leap into entrepreneurship and founded Far West Capital, which
+                    had a successful bank exit in 2018 after supporting hundreds of companies’ growth.
+                </div>
             </div>
             <div class="col-md-4" style="padding: 50px 40px;
             margin: 50px;
@@ -134,7 +146,7 @@ border-radius: 4px;
 background-color: #ffffff;">
                 <div class="row">
                     <div class="col">
-                <img src="{{ asset('images/debra-headshot_sq_web_small.png') }}">
+                        <img src="{{ asset('images/debra-headshot_sq_web_small.png') }}">
                     </div>
                     <div class="col">
                 <div style="
@@ -151,11 +163,14 @@ background-color: #ffffff;">
                 <div style="margin-top: 35px;
                 color: #011f4b;
                 font-family: 'Nunito', sans-serif;
-                font-size: 25px;
-                font-weight: 400;
-                line-height: 37px;
-                letter-spacing: -0.25px;">Debra Zukonik is a well-known and respected leader in factoring and asset-based lending, with over 30 years’ experience. Her risk-management expertise is unparalleled in the working capital industry. Prior to joining Dare Capital as Chief Credit Officer, she was the President and Partner of Vertex Financial, a leading Texas-based factor, from 2001 to 2017, and served as Chief Credit Officer of BAMFi
-                    from 2017 to 2019.</div>
+                font-size: 2.5vh;
+                font-weight: 400;">Debra Zukonik is a well-known and respected leader in factoring and asset-based
+                    lending, with over 30 years’ experience. Her risk-management expertise is unparalleled in the
+                    working capital industry. Prior to joining Dare Capital as Chief Credit Officer, she was the
+                    President and Partner of Vertex Financial, a leading Texas-based factor, from 2001 to 2017, and
+                    served as Chief Credit Officer of BAMFi
+                    from 2017 to 2019.
+                </div>
             </div>
         </div>
         @include('includes.get-started')
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,30 +1,30 @@
 <div style="float: right; margin-top: 2%;">
-    <a style="width: 75px;
-                    height: 15px;
+    <a style="
                     color: #ffffff;
                     font-family: 'Nunito', sans-serif;
                     font-size: 18px;
                     font-weight: 400;
                     line-height: 30px;
                     margin-right: 50px;" href="{{ route('about') }}">About Us</a>
-    <a style="width: 75px;
-                    height: 15px;
+    <a style="
                     color: #ffffff;
                     font-family: 'Nunito', sans-serif;
                     font-size: 18px;
                     font-weight: 400;
                     line-height: 30px;margin-right: 50px;">Resources</a>
-    <a style="width: 75px;
-                    height: 15px;
+    <a style="
                     color: #ffffff;
                     font-family: 'Nunito', sans-serif;
                     font-size: 18px;
                     font-weight: 400;
                     line-height: 30px;margin-right: 50px;">Stories</a>
     <a style="margin-right: 35px;font-size: x-large;
-color: #ffffff;" href="https://www.twitter.com/coledoescapital" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter"></i></a>
+color: #ffffff;" href="https://www.twitter.com/coledoescapital" target="_blank" rel="noopener noreferrer"><i
+            class="fab fa-twitter"></i></a>
     <a style="margin-right: 35px;font-size: x-large;
-color: #ffffff;" href="https://www.facebook.com/DareBizCapital" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
+color: #ffffff;" href="https://www.facebook.com/DareBizCapital" target="_blank" rel="noopener noreferrer"><i
+            class="fab fa-facebook-f"></i></a>
     <a style="margin-right: 100px;font-size: x-large;
 color: #ffffff;" href="https://www.linkedin.com/company/darecapital" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
 </div>
+
