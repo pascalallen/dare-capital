005facb182919184312b63a0ebec55b9c13ee0d5
Pascal Allen <pascal@onlinemeded.org>
2020-01-27 19:24:29 -0600

    add get started banner to includes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -154,29 +154,6 @@ background-color: #ffffff;">
                     from 2017 to 2019.</div>
             </div>
         </div>
-        <div class="row" style="margin-top: 75px;">
-            <div class="col-md-8 offset-md-2" style="
-        padding: 60px;
-box-shadow: 0 8px 40px rgba(18, 18, 76, 0.14);
-background-color: #ffffff;
-            color: #6e748e;
-            font-family: 'Nunito', sans-serif;
-            font-size: 30px;
-            line-height: 34px;">
-            <span style="
-            font-weight: 900;">Get Started Now.</span> <span style="
-            font-family: 'Nunito', sans-serif;
-        font-weight: 400;">Apply for Funding.</span>
-                    <button style="
-        margin-left: 75px;
-        box-shadow: 0 10px 25px 1px rgba(1, 31, 75, 0.14);
-        background-color: #3a888b;font-family: 'Nunito', sans-serif;
-        font-size: 16px;
-        font-weight: 400;color: #ffffff;border: #3a888b;
-        width: 170px;
-height: 46px;
-        line-height: 30px;">Get Started Now</button>
-            </div>
-        </div>
+        @include('includes.get-started')
     </div>
 @endsection
