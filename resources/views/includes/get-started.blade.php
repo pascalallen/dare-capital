8bce6ccd9d2a17c928b1451ccb94c35f739479c5
Pascal Allen <pascal@onlinemeded.org>
2020-01-28 18:40:06 -0600

    padding for get started button

--- resources/views/includes/get-started.blade.php
+++ resources/views/includes/get-started.blade.php
@@ -1,3 +1,10 @@
+<style>
+    @media (max-width: 576px) {
+        .get-started-now-button {
+            margin-top: 25px;
+        }
+    }
+</style>
 <div class="row text-center" style="margin-top: 75px;">
     <div class="col-md-8 offset-md-2" style="
         padding: 60px;
@@ -11,13 +18,14 @@ background-color: #ffffff;
             font-weight: 900;">Get Started Now.</span> <span style="
             font-family: 'Nunito', sans-serif;
         font-weight: 400;">Apply for Funding.</span>
-        <button style="
+        <button class="get-started-now-button" style="
         box-shadow: 0 10px 25px 1px rgba(1, 31, 75, 0.14);
         background-color: #3a888b;font-family: 'Nunito', sans-serif;
         font-size: 16px;
         font-weight: 400;color: #ffffff;border: #3a888b;
         width: 170px;
 height: 46px;
-        line-height: 30px;">Get Started Now</button>
+        line-height: 30px;">Get Started Now
+        </button>
     </div>
 </div>
