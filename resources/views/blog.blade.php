3ceb8527173d65825cd4d1cb685be169f2bdb5d9
Pascal Allen <pascal@onlinemeded.org>
2020-01-28 18:04:35 -0600

    refactor blog page

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,56 +1,106 @@
 @extends('layouts.app')
 
 @section('title')
-    Blog
+    Stories
 @endsection
 
-@push('styles')
-    <style>
-        @media (max-width: 576px) {
-            .banner-image {
-                height: 163px;
-            }
-        }
-    </style>
-@endpush
-
 @section('content')
-    <div class="container mw-100 m-0 pb-5">
-
-        <div class="row mb-5">
-            <img src="{{ asset('/images/people-sunrise.jpg') }}" alt="Stories"
-                 class="banner-image"/>
-        </div>
-
-        <div class="row mb-5 justify-content-center text-light">
-            <h1 class="display-5">Stories</h1>
-        </div>
-
+    <div
+        style="background-image: url('images/2-layers.png'); background-repeat: no-repeat; background-size: cover; height: 55vw;">
+        <img style="margin-top: 4%; margin-left: 5%; width: 15%;
+  max-width: 400px;
+  min-width: 90px;
+  height: auto;" src="{{ asset('images/LOGO_Vector_Smart_Object.png') }}"
+             alt="Dare Logo" xmlns="http://www.w3.org/1999/html">
         @foreach($posts as $post)
-            <div class="row mb-5 blog-index-card justify-content-center text-light"
-                 onclick="window.location.href = '/blog/{{ $post->slug }}';">
-                <div class="col-md-4 my-auto">
-                    <div class="card bg-light shadow-lg mb-2 border-0">
-                        <img class="card-img blog-index-image"
-                             src="{{ asset('storage/images/'.$post->image) }}"
-                             alt="{{ $post->image }}">
-                        <div class="card-img-overlay text-dark">
-                            <h1 class="display-5 card-title">{{ $post->title }}</h1>
+            @if ($loop->odd)
+                <div class="row" style="margin: 50px;">
+                    <div class="col-md-7 mx-auto" style="padding: 50px 40px;
+                box-shadow: 0 10px 25px 1px rgba(20, 20, 80, 0.14);
+                border-radius: 4px;
+                background-color: #ffffff;">
+                        <div class="row">
+                            <div class="col">
+                                <div style="
+                color: #3a888b;
+                font-family: 'Nunito', sans-serif;
+                font-size: 3vh;
+                font-weight: 900;
+                line-height: 30px;
+                text-transform: uppercase;
+                letter-spacing: 0.87px;">{{ $post->title }}</div>
+                                <div style="
+                margin-top: 15px;
+                color: #011f4b;
+                font-family: 'Nunito', sans-serif;
+                font-size: 2.5vh;
+                font-weight: 300;">
+                                    <span style="font-weight: 900;">{{ $post->title }}</span>
+                                    <br>
+                                    {!! str_limit(strip_tags($post->body), $limit = 500, $end = '...') !!}
+                                </div>
+
+                            </div>
+                            <div class="col"><img class="img-thumbnail" style="min-width: 200px;"
+                                                  src="{{ asset('storage/images/'.$post->image) }}"
+                                                  alt="{{ $post->image }}"></div>
+                        </div>
+                        <div class="row">
+                            <a href="/blog/{{ $post->slug }}" style="padding-top: 15px;padding-left: 15px;
+                        color: #3c8685;
+                        font-family: 'Nunito', sans-serif;
+                        font-size: 22px;
+                        font-weight: 400;
+                        line-height: 28px;
+                        letter-spacing: 0.66px;">Read More >></a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <blockquote class="blockquote card-text">
-                        {!! str_limit(strip_tags($post->body), $limit = 500, $end = '...') !!}
-                        <footer
-                            class="blockquote-footer text-right card-text mt-3">{{ $post->user->name }}
-                            on {{ date_format($post->created_at, 'M d, Y') }}</footer>
-                    </blockquote>
-                </div>
-            </div>
-        @endforeach
+            @else
+                <div class="row" style="margin: 50px;">
+                    <div class="col-md-7 mx-auto" style="padding: 50px 40px;
+                box-shadow: 0 10px 25px 1px rgba(20, 20, 80, 0.14);
+                border-radius: 4px;
+                background-color: #ffffff;">
+                        <div class="row">
+                            <div class="col"><img class="img-thumbnail" style="min-width: 200px;"
+                                                  src="{{ asset('storage/images/'.$post->image) }}"
+                                                  alt="{{ $post->image }}"></div>
+                            <div class="col">
+                                <div style="
+                color: #3a888b;
+                font-family: 'Nunito', sans-serif;
+                font-size: 3vh;
+                font-weight: 900;
+                line-height: 30px;
+                text-transform: uppercase;
+                letter-spacing: 0.87px;">{{ $post->title }}</div>
+                                <div style="
+                margin-top: 15px;
+                color: #011f4b;
+                font-family: 'Nunito', sans-serif;
+                font-size: 2.5vh;
+                font-weight: 300;">
+                                    <span style="font-weight: 900;">{{ $post->title }}</span>
+                                    <br>
+                                    {!! str_limit(strip_tags($post->body), $limit = 500, $end = '...') !!}
+                                </div>
 
-        {{ $posts->links() }}
+                            </div>
+                        </div>
+                        <div class="row justify-content-end">
+                            <a href="/blog/{{ $post->slug }}" style="padding-top: 15px;padding-right: 15px;
+                        color: #3c8685;
+                        font-family: 'Nunito', sans-serif;
+                        font-size: 22px;
+                        font-weight: 400;
+                        line-height: 28px;
+                        letter-spacing: 0.66px;">Read More >></a>
+                        </div>
+                    </div>
+                </div>
+            @endif
+        @endforeach
     </div>
 
 @endsection
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -17,7 +17,7 @@
                     font-family: 'Nunito', sans-serif;
                     font-size: 18px;
                     font-weight: 400;
-                    line-height: 30px;margin-right: 50px;">Stories</a>
+                    line-height: 30px;margin-right: 50px;" href="{{ route('blog') }}">Stories</a>
     <a style="margin-right: 35px;font-size: x-large;
 color: #ffffff;" href="https://www.twitter.com/coledoescapital" target="_blank" rel="noopener noreferrer"><i
             class="fab fa-twitter"></i></a>
@@ -25,6 +25,7 @@ color: #ffffff;" href="https://www.twitter.com/coledoescapital" target="_blank" 
 color: #ffffff;" href="https://www.facebook.com/DareBizCapital" target="_blank" rel="noopener noreferrer"><i
             class="fab fa-facebook-f"></i></a>
     <a style="margin-right: 100px;font-size: x-large;
-color: #ffffff;" href="https://www.linkedin.com/company/darecapital" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+color: #ffffff;" href="https://www.linkedin.com/company/darecapital" target="_blank" rel="noopener noreferrer"><i
+            class="fab fa-linkedin-in"></i></a>
 </div>
 
