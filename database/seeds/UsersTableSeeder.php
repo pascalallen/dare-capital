3ff0ef2b10483f721564d9f663cad56fb0f8fb3f
Pascal Allen <pascal@onlinemeded.org>
2020-01-29 10:37:11 -0600

    update users seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -27,7 +27,7 @@ class UsersTableSeeder extends Seeder
             [
                 'name' => 'Cole Harmonson',
                 'email' => 'cole@darebizcapital.com',
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'bio' => 'From the very beginning of his career, Cole has recognized the transformative value of factoring and asset-based lending to manage cashflow for growing companies without impacting equity. In 1996, he founded State Bank’s factoring and asset based lending business; when they sold their business in 2007, Cole took the leap into entrepreneurship and founded Far West Capital, which had a successful bank exit in 2018 after supporting hundreds of companies’ growth.',
                 'avatar' => 'cole-harmonson.png',
                 'title' => 'co-founder and CEO',
@@ -37,7 +37,7 @@ class UsersTableSeeder extends Seeder
             [
                 'name' => 'Debra Zukonik',
                 'email' => 'debra@darebizcapital.com',
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'bio' => 'Debra Zukonik is a well-known and respected leader in factoring and asset-based lending, with over 30 years’ experience. Her risk-management expertise is unparalleled in the working capital industry. Prior to joining Dare Capital as Chief Credit Officer, she was the President and Partner of Vertex Financial, a leading Texas-based factor, from 2001 to 2017, and served as Chief Credit Officer of BAMFi from 2017 to 2019.',
                 'avatar' => 'debra-zukonik.png',
                 'title' => 'co-founder and CCO',
