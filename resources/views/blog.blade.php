9eec98a97485c094016e64d13ac8f63111df623a
Pascal Allen <pascal@onlinemeded.org>
2020-01-30 11:41:52 -0600

    update blog landing picture

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <div
-        style="background-image: url('images/2-layers.png'); background-repeat: no-repeat; background-size: cover; height: 55vw;">
+        style="background-image: url('images/header_pic_construction_blue_overlay_web.png'); background-repeat: no-repeat; background-size: cover; height: 55vw;">
         <img style="margin-top: 4%; margin-left: 5%; width: 15%;
   max-width: 400px;
   min-width: 90px;
