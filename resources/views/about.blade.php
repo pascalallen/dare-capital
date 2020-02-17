481061cabe567bebea046762449819f20654e421
Pascal Allen <pascal@onlinemeded.org>
2020-02-17 15:07:28 -0600

    cleanup about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -13,12 +13,11 @@
             45vw
         @endslot
     @endcomponent
-    <div class="row about-section-row">
-        <div class="col-md-8 mx-auto about-section-col">
-            <div class="about-section-header">WHO WE ARE</div>
-            <div class="about-section-body">
-                <span
-                    class="about-section-main-phrase">We’re the financial partner you’ve always wanted on your team. </span>
+    <div class="row about-page-section">
+        <div class="col-md-8 mx-auto about-page-section-col">
+            <div class="about-page-section-header">WHO WE ARE</div>
+            <div class="about-page-section-body">
+                <strong>We’re the financial partner you’ve always wanted on your team. </strong>
                 We have a combined 60 years of experience helping entrepreneurs build and grow; we’ve lent over 6
                 billion dollars to fund working capital needs. Obstacles? Complications? You name it, we’ve seen it
                 - and overcome it. We love forming long-term relationships with growing companies; we especially
@@ -27,23 +26,25 @@
             </div>
         </div>
     </div>
-    <div class="row about-section-row">
-        <div class="col-md-8 mx-auto about-section-col">
-            <div class="about-section-header">WHAT WE DO</div>
-            <div class="about-section-body">
+    <div class="row about-page-section">
+        <div class="col-md-8 mx-auto about-page-section-col">
+            <div class="about-page-section-header">WHAT WE DO</div>
+            <div class="about-page-section-body">
                 Do you need working capital to grow? Have outstanding invoices you'd love to get paid off today, not
                 in 30 or 60 days? We factor accounts receivables and provide lines of credit for growing companies.
-                <span class="about-section-main-phrase">We’ll be there to help you grow and scale; we’ll be there when you need advice or an introduction; and when there are problems, we strive to be your first call.</span>
+                <strong>We’ll be there to help you grow and scale; we’ll be there when you need advice or an
+                    introduction; and when there are problems, we strive to be your first call.</strong>
                 We’ll always tell you the truth−good news and bad news−and share everything that informs our
                 decision making.
             </div>
         </div>
     </div>
-    <div class="row about-section-row">
-        <div class="col-md-8 mx-auto about-section-col">
-            <div class="about-section-header">WHY WE DO IT</div>
-            <div class="about-section-body">
-                <span class="about-section-main-phrase">We’re really good at it! We’ve grown thousands of companies, most of whom have worked with us for years. And we know what it takes.</span>
+    <div class="row about-page-section">
+        <div class="col-md-8 mx-auto about-page-section-col">
+            <div class="about-page-section-header">WHY WE DO IT</div>
+            <div class="about-page-section-body">
+                <strong>We’re really good at it! We’ve grown thousands of companies, most of whom have worked with us
+                    for years. And we know what it takes.</strong>
                 We’ve seen that successful businesses are built from a giving mindset, by courageous entrepreneurs
                 who know their vulnerabilities, own their failures, and turn both into their biggest strengths.
                 We've seen that the best measure of long-term success is in how a business treats its smallest
@@ -54,36 +55,36 @@
         </div>
     </div>
     <img src="{{ asset('images/Vector_Smart_Object_Diagonal_Line_1.png') }}" class="w-100" alt="Dare Capital">
-    <div class="row justify-content-center text-center core-values-row">
-        <div class="col-md-4 core-values-col">
-            <div class="core-values-header">"A Team" Effort</div>
-            <div class="core-values-body">
-                <span class="core-values-main-phrase d-block">We are more together than we are alone.</span>
+    <div class="row justify-content-center text-center about-page-section">
+        <div class="col-md-4 about-page-section-col">
+            <div class="about-page-section-header">"A Team" Effort</div>
+            <div class="about-page-section-body">
+                <strong class="d-block">We are more together than we are alone.</strong>
                 Our team is committed to working in lockstep with each other and our clients toward consistent long-term
                 success.
             </div>
         </div>
-        <div class="col-md-4 core-values-col">
-            <div class="core-values-header">Dare to Give, First</div>
-            <div class="core-values-body">
-                <span class="core-values-main-phrase d-block">In Latin, "dare" means "to give."</span>
+        <div class="col-md-4 about-page-section-col">
+            <div class="about-page-section-header">Dare to Give, First</div>
+            <div class="about-page-section-body">
+                <strong class="d-block">In Latin, "dare" means "to give."</strong>
                 We believe in giving our best, putting our best foot forward, and working with clients who dare to give
                 trust, transparency, and match our best with their own.
             </div>
         </div>
     </div>
-    <div class="row justify-content-center text-center core-values-row">
-        <div class="col-md-4 core-values-col">
-            <div class="core-values-header">Further & Onwards</div>
-            <div class="core-values-body">
-                <span class="core-values-main-phrase d-block">There is always another improvement to make.</span>
+    <div class="row justify-content-center text-center about-page-section">
+        <div class="col-md-4 about-page-section-col">
+            <div class="about-page-section-header">Further & Onwards</div>
+            <div class="about-page-section-body">
+                <strong class="d-block">There is always another improvement to make.</strong>
                 We take a growth mindset approach to our business and our clients' success.
             </div>
         </div>
-        <div class="col-md-4 core-values-col">
-            <div class="core-values-header">Radical Transparency</div>
-            <div class="core-values-body">
-                <span class="core-values-main-phrase d-block">We strive to never surprise anyone with anything.</span>
+        <div class="col-md-4 about-page-section-col">
+            <div class="about-page-section-header">Radical Transparency</div>
+            <div class="about-page-section-body">
+                <strong class="d-block">We strive to never surprise anyone with anything.</strong>
                 We will always share information − good or bad − with each other and with our clients, explaining every
                 decision we make to the best of our ability.
             </div>
@@ -91,65 +92,45 @@
     </div>
     <img src="{{ asset('images/Vector_Smart_Object_Diagonal_Line_1.png') }}" class="w-100" alt="Dare Capital">
     <h1 class="display-4 text-center">MEET OUR MANAGEMENT</h1>
-    <div class="row justify-content-center mr-0 ml-0">
-        <div class="col-md-4" style="padding: 50px 40px;
-        margin: 50px 15px;
-box-shadow: 0 10px 25px 1px rgba(20, 20, 80, 0.14);
-border-radius: 4px;
-background-color: #ffffff;">
-            <img src="{{ asset('storage/avatars/cole-harmonson.png') }}">
-            <div style="
-            color: #3a888b;
-            font-size: 28px;
-            line-height: 25px;
-            margin-top: 15px;
-            font-weight: 900;
-            display: inline-block;
-            letter-spacing: 0.84px;">COLE HARMONSON<br>
-                <span style="font-weight: 400;letter-spacing: 0.84px;">co-founder and CEO</span>
+    <div class="row justify-content-center about-page-section">
+        <div class="col-md-4 about-page-section-col">
+            <img src="{{ asset('storage/avatars/cole-harmonson.png') }}" alt="Dare Capital">
+            <div class="about-page-section-header">
+                COLE HARMONSON
+                <br>
+                <span>co-founder and CEO</span>
             </div>
-            <div style="margin-top: 5px;
-            color: #011f4b;
-            font-size: 18px;
-            font-weight: 400;">From the very beginning of his career, Cole has recognized the transformative value
+            <div class="about-page-bio-body">
+                From the very beginning of his career, Cole has recognized the transformative value
                 of factoring and asset-based lending to manage cashflow for growing companies without impacting
                 equity. In 1996, he founded State Bank’s factoring and asset based lending business; when they sold
                 their business in 2007, Cole took the leap into entrepreneurship and founded Far West Capital, which
                 had a successful bank exit in 2018 after supporting hundreds of companies’ growth.
+                <br>
+                <br>
+                <a href="mailto:cole@darebizcapital.com">Email Cole here</a>, or give him a call
+                at <a href="tel:1-512-886-3272">512-886-DARE x 101</a>
             </div>
-            <br>
-            <a href="mailto:cole@darebizcapital.com" style="color: #3a888b;">Email Cole here</a>, or give him a call
-            at <a href="tel:1-512-886-3272" style="color: #3a888b;">512-886-DARE x 101</a>
         </div>
-        <div class="col-md-4" style="padding: 50px 40px;
-        margin: 50px 15px;
-box-shadow: 0 10px 25px 1px rgba(20, 20, 80, 0.14);
-border-radius: 4px;
-background-color: #ffffff;">
-            <img src="{{ asset('storage/avatars/debra-zukonik.png') }}">
-            <div style="
-            color: #3a888b;
-            font-size: 28px;
-            line-height: 25px;
-            margin-top: 15px;
-            font-weight: 900;
-            display: inline-block;
-            letter-spacing: 0.84px;">DEBRA ZUKONIK<br>
-                <span style="font-weight: 400;letter-spacing: 0.84px;">co-founder and CCO</span>
+        <div class="col-md-4 about-page-section-col">
+            <img src="{{ asset('storage/avatars/debra-zukonik.png') }}" alt="Dare Capital">
+            <div class="about-page-section-header">
+                DEBRA ZUKONIK
+                <br>
+                <span>co-founder and CCO</span>
             </div>
-            <div style="margin-top: 5px;
-            color: #011f4b;
-            font-size: 18px;
-            font-weight: 400;">Debra Zukonik is a well-known and respected leader in factoring and asset-based
+            <div class="about-page-bio-body">
+                Debra Zukonik is a well-known and respected leader in factoring and asset-based
                 lending, with over 30 years’ experience. Her risk-management expertise is unparalleled in the
                 working capital industry. Prior to joining Dare Capital as Chief Credit Officer, she was the
                 President and Partner of Vertex Financial, a leading Texas-based factor, from 2001 to 2017, and
                 served as Chief Credit Officer of BAMFi
                 from 2017 to 2019.
+                <br>
+                <br>
+                <a href="mailto:debra@darebizcapital.com">Email Debra here</a>, or give her a
+                call at <a href="tel:1-214-329-9922">512-886-DARE x 102</a>
             </div>
-            <br>
-            <a href="mailto:debra@darebizcapital.com" style="color: #3a888b;">Email Debra here</a>, or give her a
-            call at <a href="tel:1-214-329-9922" style="color: #3a888b;">512-886-DARE x 102</a>
         </div>
     </div>
     @include('includes.visit-blog')
