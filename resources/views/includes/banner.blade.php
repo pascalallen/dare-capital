04ff5e72a3287d5637f9bf8e0a633076982ec240
Pascal Allen <pascal.allen88@gmail.com>
2020-04-01 11:45:32 -0500

    covid banner (#22)
    Co-authored-by: Pascal Allen <pascal@onlinemeded.org>

---
+++ resources/views/includes/banner.blade.php
@@ -0,0 +1,6 @@
+{{-- TODO: delete file when banner should be removed --}}
+<a href="https://bit.ly/darecapcovidguidesmb" class="event-banner">
+    <div class="jumbotron mb-0 py-2 text-white text-center">
+        <p class="lead mb-0">BUSINESS OWNERS: Get our guide to COVID-19 recovery here</p>
+    </div>
+</a>
