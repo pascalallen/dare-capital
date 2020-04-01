85ecce45557616be799838f7f1c102b74b6c42d0
Pascal Allen <pascal@onlinemeded.org>
2020-04-01 11:49:29 -0500

    open new tab

--- resources/views/includes/banner.blade.php
+++ resources/views/includes/banner.blade.php
@@ -1,5 +1,5 @@
 {{-- TODO: delete file when banner should be removed --}}
-<a href="https://bit.ly/darecapcovidguidesmb" class="event-banner">
+<a href="https://bit.ly/darecapcovidguidesmb" class="event-banner" target="_blank" rel="noopener noreferrer">
     <div class="jumbotron mb-0 py-2 text-white text-center">
         <p class="lead mb-0">BUSINESS OWNERS: Get our guide to COVID-19 recovery here</p>
     </div>
