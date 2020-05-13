8b2ac2b7072bc822675fb7eeeabbc2e20037851a
Pascal Allen <pascal@onlinemeded.org>
2020-05-12 19:55:16 -0500

    add test email

---
+++ resources/views/emails/application.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title')
+    Test
+@endsection
+
+@section('content')
+    <div class="row">[THIS IS A TEST]Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias consequatur consequuntur dolor, doloremque ea eos inventore labore maxime perferendis repellendus saepe, sint sit voluptates. Accusantium dicta dolorem facilis nisi porro?</div>
+@endsection
